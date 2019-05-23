2cdbfa5587262b9f8118f054bfaf2d32c1026e90
Chen Ruofan <physcrf@qq.com>
2019-05-23 11:49:01 +0800

    aux

--- src/package.lisp
+++ src/package.lisp
@@ -1,6 +1,7 @@
 (defpackage :bib
   (:use :cl :utility :string :os :cl-ppcre :gref)
   (:export :parse-bibfile :format-bibitem
+	   :parse-auxfile
 	   :find-journal :search-journal
 	   :list-journals :add-journal :add-journals
 	   :*abbrev-table*))
